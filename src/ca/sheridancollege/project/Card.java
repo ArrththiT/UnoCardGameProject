a5a8ab997d1e2035f76cd49d4a77000403682552
Arrththi Thavabalanathan <thavabaa@sheridancollege.ca>
2021-12-13 15:57:47 -0500

    4th commit

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -8,18 +8,37 @@ package ca.sheridancollege.project;
 /**
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *code
+ *
  * @author dancye
  */
-public abstract class Card {
+public class Card {
     //default modifier for child classes
+    private Cards cards;
+    private Color color;
 
     /**
      * Students should implement this method for their specific children classes
      *
      * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
      */
-    @Override
-    public abstract String toString();
+    public Card (Cards cards, Color color)
+   {
+      this.cards = cards;
+      this.color = color;
+   }
 
+   public Cards getCardNumber ()
+   {
+      return this.cards;
+   }
+
+   public Color getCardColor ()
+   {
+      return this.color;
+   }
+
+   public void setCardColor (Color color)
+   {
+      this.color = color;
+   }
 }
