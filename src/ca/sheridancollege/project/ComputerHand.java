8b503772d019ee4981e96272d9e136dbde2f4221
Arrththi Thavabalanathan <thavabaa@sheridancollege.ca>
2021-12-13 16:55:22 -0500

    added void cards (someone please work on this!)

--- src/ca/sheridancollege/project/ComputerHand.java
+++ src/ca/sheridancollege/project/ComputerHand.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
@@ -29,5 +26,8 @@ public class ComputerHand extends GroupOfCards
    public int getComputerSize ()
    {
       return this.computerHand.size();
+   }
+   void addCards(){
+       
    }
 }
