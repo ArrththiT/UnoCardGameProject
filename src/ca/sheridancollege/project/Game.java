90e2ed1644455601c9abe120b18d035a01fb1f07
Arrththi Thavabalanathan <thavabaa@sheridancollege.ca>
2021-12-13 15:59:11 -0500

    7th commit

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -6,6 +6,7 @@
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * The class that models your game. You should create a more specific child of this class and instantiate the methods
@@ -15,9 +16,12 @@ import java.util.ArrayList;
  * @author Paul Bonenfant Jan 2020
  */
 public abstract class Game {
+    
 
     private final String name;//the title of the game
     private ArrayList<Player> players;// the players of the game
+    
+
 
     public Game(String name) {
         this.name = name;
@@ -46,13 +50,43 @@ public abstract class Game {
     }
 
     /**
-     * Play the game. This might be one method or many method calls depending on your game.
+     * Play the game.This might be one method or many method calls depending on your game.
+     * @param cardH
+     * @param ch
+     * @param us
      */
+    public void ComputerDrawFour(ArrayList<Card> cardH, ComputerHand ch, UserHand us){
+        for(int i = 0; i<cardH.size(); i++){
+            if (cardH.get(i).getCardNumber()==Cards.DRAWFOUR){
+                System.out.println("Opponent will pick up 4 cards");
+                //adding cards to user hand
+                
+
+            }
+      
+        }
+ 
+    }
+    
+    
+    public void userDrawFour(ArrayList<Card> cardH, ComputerHand ch, UserHand us ){
+        for(int i = 0; i<cardH.size(); i++){
+            if (cardH.get(i).getCardNumber()==Cards.DRAWFOUR){
+                System.out.println("Opponent will pick up 4 cards");
+                //adding cards to computer hand
+                us.getSize();
+
+            }
+      
+        }
+    }
+    
+    
     public abstract void play();
 
     /**
      * When the game is over, use this method to declare and display a winning player.
      */
-    public abstract void declareWinner();
+    public abstract void declareWinner(); 
 
 }//end class
