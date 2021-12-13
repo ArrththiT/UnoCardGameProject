f7d05d5392dc7cec29dcbdd0668426ff20d71cc7
Arrththi Thavabalanathan <thavabaa@sheridancollege.ca>
2021-12-13 14:43:28 -0500

    third commit

---
+++ src/ca/sheridancollege/project/Cards.java
@@ -0,0 +1,10 @@
+
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author BingChen Sun
+ */
+public enum Cards {
+     ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, DRAWFOUR, DRAWTWO, SKIP, WILD;
+}
